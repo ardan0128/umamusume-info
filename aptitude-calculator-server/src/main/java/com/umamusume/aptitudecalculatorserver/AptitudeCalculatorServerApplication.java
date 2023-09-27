ab1fbd19d485643fa0e93565c2233df537f7d671
ardan0128 <jwon1818@daum.net>
2023-09-27 13:36:51 +0900

    [ADD] server

---
+++ aptitude-calculator-server/src/main/java/com/umamusume/aptitudecalculatorserver/AptitudeCalculatorServerApplication.java
@@ -0,0 +1,13 @@
+package com.umamusume.aptitudecalculatorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptitudeCalculatorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptitudeCalculatorServerApplication.class, args);
+	}
+
+}
