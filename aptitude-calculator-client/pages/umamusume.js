dbb7440ee11936d08a99307bdef100b28d3332c2
ardan0128 <jwon1818@daum.net>
2023-10-01 21:12:45 +0900

    [ADD] aptitude-calculator-client

---
+++ aptitude-calculator-client/pages/umamusume.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function test() {
+  return (
+    <div>
+      <div>hello</div>
+      <Link href="/umamusume/mejiro-ardan">메지로 아르당</Link>
+      <Link href="/umamusume">back</Link>
+      <Link href="/umamusume">back</Link>
+      <Link href="/umamusume">back</Link>
+      <Link href="/umamusume">back</Link>
+      <Link href="/umamusume">back</Link>
+    </div>
+  );
+}
---
+++ aptitude-calculator-client/pages/umamusume/mejiro-ardan.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function test() {
+  return (
+    <div>
+      <div>hello ardan</div>
+      <div>
+        <Link href="/umamusume">back</Link>
+      </div>
+    </div>
+  );
+}
