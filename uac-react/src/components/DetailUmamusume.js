14d229ff6581e3a6785442b1cfc93e60bffb0984
ardan0128 <jwon1818@daum.net>
2023-11-06 23:37:21 +0900

    [FEAT] DetailUmamusume component 작업 중

---
+++ uac-react/src/components/DetailUmamusume.js
@@ -0,0 +1,81 @@
+function Detailumamusume(props) {
+  console.log(props);
+  return (
+    <>
+      <div>
+        <div>{props.title}</div>
+        <div>
+          <img src={props.image} alt={props.id}></img>
+        </div>
+        <div>{props.name}</div>
+        <div>
+          <div>경기장 적성</div>
+          <div>
+            <div>잔디</div>
+            <div>{props.surface.turf}</div>
+          </div>
+          <div>
+            <div>더트</div>
+            <div>{props.surface.dirt}</div>
+          </div>
+        </div>
+        <div>
+          <div>거리 적성</div>
+          <div>
+            <div>
+              <div>단거리</div>
+              <div>{props.distance.short}</div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>마일</div>
+              <div>{props.distance.mile}</div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>중거리</div>
+              <div>{props.distance.medium}</div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>장거리</div>
+              <div>{props.distance.long}</div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>각질 적성</div>
+          <div>
+            <div>
+              <div>도주</div>
+              <div>{props.strategy.runner}</div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>선행</div>
+              <div>{props.strategy.leader}</div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>선입</div>
+              <div>{props.strategy.betweener}</div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>추입</div>
+              <div>{props.strategy.chaser}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Detailumamusume;
--- uac-react/src/pages/DetailUmamusume.js
+++ uac-react/src/pages/DetailUmamusume.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Detailumamusume from '../components/DetailUmamusume';
 import { getDetailUmamusume } from '../data/api/Umamusume';
+import { useEffect, useState } from 'react';
 
 function DetailUmamusume() {
   const params = useParams();
   const [loading, setLoading] = useState(true);
   const [umamusumeDetail, setUmamusumeDetail] = useState();
   const [image, setImage] = useState();
-  // const umamusumeImage = require(`${process.env.REACT_APP_PUBLIC_IMAGES}/${umamusumeDetail.id}.png`);
 
   useEffect(() => {
     getDetailUmamusume(params.id).then(res => {
@@ -15,88 +15,9 @@ function DetailUmamusume() {
       setUmamusumeDetail(res);
       setImage(require(`${process.env.REACT_APP_PUBLIC_IMAGES}/${res.id}.png`));
     });
-  }, []);
+  }, [params.id]);
 
-  return (
-    <>
-      {loading ? (
-        <div>loading...</div>
-      ) : (
-        <div>
-          <div>{umamusumeDetail.title}</div>
-          <div>
-            <img src={image} alt={umamusumeDetail.id}></img>
-          </div>
-          <div>{umamusumeDetail.name}</div>
-          <div>
-            <div>경기장 적성</div>
-            <div>
-              <div>잔디</div>
-              <div>{umamusumeDetail.surface.turf}</div>
-            </div>
-            <div>
-              <div>더트</div>
-              <div>{umamusumeDetail.surface.dirt}</div>
-            </div>
-          </div>
-          <div>
-            <div>거리 적성</div>
-            <div>
-              <div>
-                <div>단거리</div>
-                <div>{umamusumeDetail.distance.short}</div>
-              </div>
-            </div>
-            <div>
-              <div>
-                <div>마일</div>
-                <div>{umamusumeDetail.distance.mile}</div>
-              </div>
-            </div>
-            <div>
-              <div>
-                <div>중거리</div>
-                <div>{umamusumeDetail.distance.medium}</div>
-              </div>
-            </div>
-            <div>
-              <div>
-                <div>장거리</div>
-                <div>{umamusumeDetail.distance.long}</div>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div>각질 적성</div>
-            <div>
-              <div>
-                <div>도주</div>
-                <div>{umamusumeDetail.strategy.runner}</div>
-              </div>
-            </div>
-            <div>
-              <div>
-                <div>선행</div>
-                <div>{umamusumeDetail.strategy.leader}</div>
-              </div>
-            </div>
-            <div>
-              <div>
-                <div>선입</div>
-                <div>{umamusumeDetail.strategy.betweener}</div>
-              </div>
-            </div>
-            <div>
-              <div>
-                <div>추입</div>
-                <div>{umamusumeDetail.strategy.chaser}</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+  return <>{loading ? `loding...` : <Detailumamusume {...umamusumeDetail} image={image}></Detailumamusume>}</>;
 }
 
 export default DetailUmamusume;
