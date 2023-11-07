bcd902d3f95977974a33ac5dd375dcbb65af0926
ardan0128 <jwon1818@daum.net>
2023-11-07 14:36:52 +0900

    [FEAT] DetailUmamusume 페이지 수정

--- uac-react/src/pages/DetailUmamusume.js
+++ uac-react/src/pages/DetailUmamusume.js
@@ -7,13 +7,11 @@ function DetailUmamusume() {
   const params = useParams();
   const [loading, setLoading] = useState(true);
   const [umamusumeDetail, setUmamusumeDetail] = useState();
-  const [image, setImage] = useState();
 
   useEffect(() => {
     getDetailUmamusume(params.id).then(res => {
       setLoading(false);
       setUmamusumeDetail(res);
-      setImage(require(`${process.env.REACT_APP_PUBLIC_IMAGES}/${res.id}.png`));
     });
   }, [params.id]);
 
