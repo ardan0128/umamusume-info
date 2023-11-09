2b749a02e15b47ddd4b82fb8c9027dd88262910e
ardan0128 <jwon1818@daum.net>
2023-11-09 14:29:46 +0900

    [FEAT] ListUmamusume tailwind css

--- uac-react/src/components/Umamusume.js
+++ uac-react/src/components/Umamusume.js
@@ -2,9 +2,9 @@ function Umamusume(props) {
   return (
     <div>
       <div>
-        <img src={`${process.env.PUBLIC_URL}/images/umamusume/${props.id}.png`} alt={props.id}></img>
+        <img className="rounded-xl bg-white" src={`${process.env.PUBLIC_URL}/images/umamusume/${props.id}.png`} alt={props.id}></img>
       </div>
-      <div>{props.name}</div>
+      <div className="flex justify-center mt-4 rounded-xl bg-white">{props.name}</div>
     </div>
   );
 }
--- uac-react/src/pages/ListUmamusume.js
+++ uac-react/src/pages/ListUmamusume.js
@@ -16,16 +16,16 @@ function ListUmamusume() {
 
   return (
     <>
-      <div className="w-full items-center">
+      <div className="w-full items-center p-4">
         <div>List Umamusume</div>
-        <div>
+        <div className="mt-4">
           <ul className="grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
             {loading
               ? 'loading...'
               : umamusumes.map(umamusume => {
                   return (
                     <li key={umamusume.id}>
-                      <div>
+                      <div className="rounded-xl bg-zinc-200 p-4">
                         <Link to={`${umamusume.id}`}>
                           <Umamusume id={umamusume.id} name={umamusume.name}></Umamusume>
                         </Link>
