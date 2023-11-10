530855923d2ecb4ec6744ec33e4aea76c35d9114
ardan0128 <jwon1818@daum.net>
2023-11-10 16:57:25 +0900

    [FEAT] DetailUmamusume css

--- uac-react/src/components/DetailUmamusume.js
+++ uac-react/src/components/DetailUmamusume.js
@@ -2,78 +2,63 @@ function Detailumamusume(props) {
   console.log(props);
   return (
     <>
-      <div className="p-4 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+      <div className="p-4 w-full">
         <div className="flex justify-center">
-          <div className="flex flex-col">
-            <div className="text-center">{props.title}</div>
-            <div>
-              <img className="max-w-80 max-h-80" src={`${process.env.PUBLIC_URL}/images/umamusume/${props.id}.png`} alt={props.id}></img>
-            </div>
-            <div className="text-center">{props.name}</div>
+          <div>
+            <img className="max-h-36" src={`${process.env.PUBLIC_URL}/images/umamusume/${props.id}.png`} alt={props.id}></img>
+          </div>
 
-            <div className="grid">
-              <div>경기장 적성</div>
-              <div className="grid grid-cols-2">
-                <div>잔디</div>
-                <div>{props.surface.turf}</div>
+          <div>
+            <div className="flex-auto text-center">{props.title}</div>
+            <div className="flex-auto text-center">{props.name}</div>
+            <div className="flex flex-row">
+              <div className="w-28 text-center">경기장 적성</div>
+              <div className="flex">
+                <div className="w-12 text-center">잔디</div>
+                <div className="text-center">{props.surface.turf}</div>
               </div>
-              <div className="grid grid-cols-2">
-                <div>더트</div>
-                <div>{props.surface.dirt}</div>
-              </div>
-            </div>
-            <div className="grid grid-cols-9">
-              <div>거리 적성</div>
-              <div className="grid-cols-2">
-                <div>
-                  <div>단거리</div>
-                  <div>{props.distance._short}</div>
-                </div>
-              </div>
-              <div>
-                <div>
-                  <div>마일</div>
-                  <div>{props.distance._mile}</div>
-                </div>
-              </div>
-              <div>
-                <div>
-                  <div>중거리</div>
-                  <div>{props.distance._medium}</div>
-                </div>
-              </div>
-              <div>
-                <div>
-                  <div>장거리</div>
-                  <div>{props.distance._long}</div>
-                </div>
+              <div className="flex">
+                <div className="w-12 text-center">더트</div>
+                <div className="text-center">{props.surface.dirt}</div>
               </div>
             </div>
-            <div>
-              <div>각질 적성</div>
-              <div>
-                <div>
-                  <div>도주</div>
-                  <div>{props.strategy.runner}</div>
-                </div>
+            <div className="flex flex-row">
+              <div className="w-28 text-center">거리 적성</div>
+              <div className="flex">
+                <div className="w-12 text-center">단거리</div>
+                <div className="text-center">{props.distance._short}</div>
               </div>
-              <div>
-                <div>
-                  <div>선행</div>
-                  <div>{props.strategy.leader}</div>
-                </div>
+              <div className="flex">
+                <div className="w-12 text-center">마일</div>
+                <div className="text-center">{props.distance._mile}</div>
               </div>
-              <div>
-                <div>
-                  <div>선입</div>
-                  <div>{props.strategy.betweener}</div>
-                </div>
+              <div className="flex">
+                <div className="w-12 text-center">중거리</div>
+                <div className="text-center">{props.distance._medium}</div>
               </div>
-              <div>
-                <div>
-                  <div>추입</div>
-                  <div>{props.strategy.chaser}</div>
-                </div>
+              <div className="flex">
+                <div className="w-12 text-center">장거리</div>
+                <div className="text-center">{props.distance._long}</div>
+              </div>
+            </div>
+
+            <div className="flex flex-row">
+              <div className="w-28 text-center">각질 적성</div>
+              <div className="flex">
+                <div className="w-12 text-center">도주</div>
+                <div>{props.strategy.runner}</div>
+              </div>
+              <div className="flex">
+                <div className="w-12 text-center">선행</div>
+                <div>{props.strategy.leader}</div>
+              </div>
+              <div className="flex">
+                <div className="w-12 text-center">선입</div>
+                <div>{props.strategy.betweener}</div>
+              </div>
+              <div className="flex">
+                <div className="w-12 text-center">추입</div>
+                <div>{props.strategy.chaser}</div>
               </div>
             </div>
           </div>
