2dcefe430c72c743b052c3be3bed8625cac90b36
ardan0128 <jwon1818@daum.net>
2023-12-12 15:56:53 +0900

    [FEAT] fix Detail page

--- uac-react/src/components/DetailUmamusume.js
+++ uac-react/src/components/DetailUmamusume.js
@@ -3,65 +3,78 @@ function Detailumamusume(props) {
     <>
       <div className="p-8 w-full min-w-640">
         <div className="flex justify-center items-center">
-          <div>
-            <img className="max-h-28" src={`${process.env.PUBLIC_URL}/images/umamusume/${props.id}.png`} alt={props.id}></img>
+          <div className="w-1/4">
+            <img src={`${process.env.PUBLIC_URL}/images/umamusume/${props.id}.png`} alt={props.id}></img>
           </div>
 
           <div>
-            <div className="flex-auto text-center">{props.title}</div>
-            <div className="flex-auto text-center">{props.name}</div>
+            <div className="flex-auto text-center text-3xl">{props.title}</div>
+            <div className="flex-auto text-center text-4xl">{props.name}</div>
             <div className="flex flex-row">
-              <div className="w-28 text-center">경기장 적성</div>
+              <div className="w-28 text-center text-lg">경기장 적성</div>
               <div className="flex">
-                <div className="w-12 text-center">잔디</div>
-                <div className="w-4 text-center">{props.surface.turf}</div>
+                <div className="w-12 text-center text-lg">잔디</div>
+                <div className="w-4 text-center text-lg">{props.surface.turf}</div>
               </div>
               <div className="flex">
-                <div className="w-12 text-center">더트</div>
-                <div className="w-4 text-center">{props.surface.dirt}</div>
+                <div className="w-12 text-center text-lg">더트</div>
+                <div className="w-4 text-center text-lg">{props.surface.dirt}</div>
               </div>
             </div>
             <div className="flex flex-row">
-              <div className="w-28 text-center">거리 적성</div>
+              <div className="w-28 text-center text-lg">거리 적성</div>
               <div className="flex">
-                <div className="w-12 text-center">단거리</div>
-                <div className="w-4 text-center">{props.distance._short}</div>
+                <div className="w-12 text-center text-lg">단거리</div>
+                <div className="w-4 text-center text-lg">{props.distance._short}</div>
               </div>
               <div className="flex">
-                <div className="w-12 text-center">마일</div>
-                <div className="w-4 text-center">{props.distance._mile}</div>
+                <div className="w-12 text-center text-lg">마일</div>
+                <div className="w-4 text-center text-lg">{props.distance._mile}</div>
               </div>
               <div className="flex">
-                <div className="w-12 text-center">중거리</div>
-                <div className="w-4 text-center">{props.distance._medium}</div>
+                <div className="w-12 text-center text-lg">중거리</div>
+                <div className="w-4 text-center text-lg">{props.distance._medium}</div>
               </div>
               <div className="flex">
-                <div className="w-12 text-center">장거리</div>
-                <div className="w-4 text-center">{props.distance._long}</div>
+                <div className="w-12 text-center text-lg">장거리</div>
+                <div className="w-4 text-center text-lg">{props.distance._long}</div>
               </div>
             </div>
 
             <div className="flex flex-row">
-              <div className="w-28 text-center">각질 적성</div>
+              <div className="w-28 text-center text-lg">각질 적성</div>
               <div className="flex">
-                <div className="w-12 text-center">도주</div>
-                <div className="w-4 text-center">{props.strategy.runner}</div>
+                <div className="w-12 text-center text-lg">도주</div>
+                <div className="w-4 text-center text-lg">{props.strategy.runner}</div>
               </div>
               <div className="flex">
-                <div className="w-12 text-center">선행</div>
-                <div className="w-4 text-center">{props.strategy.leader}</div>
+                <div className="w-12 text-center text-lg">선행</div>
+                <div className="w-4 text-center text-lg">{props.strategy.leader}</div>
               </div>
               <div className="flex">
-                <div className="w-12 text-center">선입</div>
-                <div className="w-4 text-center">{props.strategy.betweener}</div>
+                <div className="w-12 text-center text-lg">선입</div>
+                <div className="w-4 text-center text-lg">{props.strategy.betweener}</div>
               </div>
               <div className="flex">
-                <div className="w-12 text-center">추입</div>
-                <div className="w-4 text-center">{props.strategy.chaser}</div>
+                <div className="w-12 text-center text-lg">추입</div>
+                <div className="w-4 text-center text-lg">{props.strategy.chaser}</div>
               </div>
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="p-8 w-full min-w-640">
+        <div className="flex justify-center items-center">
+          <div className="min-w-[16.666667%] w-1/6">
+            <img src={`${process.env.PUBLIC_URL}/images/umamusume/${props.id}.png`} alt={props.id}></img>
+          </div>
+          <div>
+            <div className="flex-auto flex-row text-center text-4xl">[{props.title}]</div>
+            <div className="flex-auto flex-row text-center text-6xl">{props.name}</div>
+          </div>
+        </div>
+        <div className="grid"></div>
       </div>
     </>
   );
--- uac-react/src/components/detailUmamusume/ParentUmamusume.js
+++ uac-react/src/components/detailUmamusume/ParentUmamusume.js
@@ -1,45 +1,99 @@
+import SvgAptitudeGuts from '../../pages/svg/blue/SvgAptitudeGuts';
+import SvgAptitudePower from '../../pages/svg/blue/SvgAptitudePower';
 import SvgAptitudeSpeed from '../../pages/svg/blue/SvgAptitudeSpeed';
+import SvgAptitudeStamina from '../../pages/svg/blue/SvgAptitudeStamina';
+import SvgAptitudeWisdom from '../../pages/svg/blue/SvgAptitudeWisdom';
 import SvgAptitudeGreen from '../../pages/svg/green/SvgAptitudeGreen';
+import SvgAptitudeLong from '../../pages/svg/red/distance/SvgAptitudeLong';
 import SvgAptitudeMedium from '../../pages/svg/red/distance/SvgAptitudeMedium';
+import SvgAptitudeShort from '../../pages/svg/red/distance/SvgAptitudeShort';
+import SvgAptitudeRunner from '../../pages/svg/red/strategy/SvgAptitudeRunner';
+import SvgAptitudeDirt from '../../pages/svg/red/surface/SvgAptitudeDirt';
+import SvgAptitudeTurf from '../../pages/svg/red/surface/SvgAptitudeTurf';
 
 function ParentUmamusume() {
   return (
     <>
       <div className="p-8 w-full min-w-640 flex justify-center items-center">
-        <div className="grid grid-cols-2 gap-4">
-          <div className="border border-sky-400 w-72">
+        <div className="grid grid-cols-2 gap-4 w-3/4">
+          <div className="border border-sky-400 w-full">
             <div className="flex relative aspect-square justify-center items-center">
               <div className="absolute w-[42%] left-[8%]">
                 <img className="rounded-full border border-gray-400" src={`${process.env.PUBLIC_URL}/images/umamusume/100501.png`} alt={100501}></img>
-                <div className="grid grid-cols-3">
+                <div className="absolute w-[36%] -bottom-3 left-0">
                   <SvgAptitudeSpeed></SvgAptitudeSpeed>
-                  <div>
-                    <SvgAptitudeMedium></SvgAptitudeMedium>
-                  </div>
-                  <div>
-                    <SvgAptitudeGreen></SvgAptitudeGreen>
-                  </div>
+                </div>
+                <div className="absolute w-[36%] -bottom-3 left-[32.5%]">
+                  <SvgAptitudeMedium></SvgAptitudeMedium>
+                </div>
+                <div className="absolute w-[36%] -bottom-3 right-0">
+                  <SvgAptitudeGreen></SvgAptitudeGreen>
                 </div>
               </div>
               <div className="absolute w-[28%] top-[12%] right-[8%]">
                 <img className="rounded-full border border-gray-400" src={`${process.env.PUBLIC_URL}/images/umamusume/100601.png`} alt={100601}></img>
+                <div className="absolute w-[36%] -bottom-2 left-0">
+                  <SvgAptitudePower></SvgAptitudePower>
+                </div>
+                <div className="absolute w-[36%] -bottom-2 left-[32.5%]">
+                  <SvgAptitudeLong></SvgAptitudeLong>
+                </div>
+                <div className="absolute w-[36%] -bottom-2 right-0">
+                  <SvgAptitudeGreen></SvgAptitudeGreen>
+                </div>
               </div>
               <div className="absolute w-[28%] bottom-[12%] right-[8%]">
                 <img className="rounded-full border border-gray-400" src={`${process.env.PUBLIC_URL}/images/umamusume/100701.png`} alt={100701}></img>
+                <div className="absolute w-[36%] -bottom-2 left-0">
+                  <SvgAptitudeStamina></SvgAptitudeStamina>
+                </div>
+                <div className="absolute w-[36%] -bottom-2 left-[32.5%]">
+                  <SvgAptitudeShort></SvgAptitudeShort>
+                </div>
+                <div className="absolute w-[36%] -bottom-2 right-0">
+                  <SvgAptitudeGreen></SvgAptitudeGreen>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="border border-pink-400 w-72">
+          <div className="border border-pink-400 w-full">
             <div className="flex relative aspect-square justify-center items-center">
               <div className="absolute w-[42%] left-[8%]">
                 <img className="rounded-full border border-gray-400" src={`${process.env.PUBLIC_URL}/images/umamusume/100201.png`} alt={100201}></img>
+                <div className="absolute w-[36%] -bottom-3 left-0">
+                  <SvgAptitudeGuts></SvgAptitudeGuts>
+                </div>
+                <div className="absolute w-[36%] -bottom-3 left-[32.5%]">
+                  <SvgAptitudeTurf></SvgAptitudeTurf>
+                </div>
+                <div className="absolute w-[36%] -bottom-3 right-0">
+                  <SvgAptitudeGreen></SvgAptitudeGreen>
+                </div>
               </div>
               <div className="absolute w-[28%] top-[12%] right-[8%]">
                 <img className="rounded-full border border-gray-400" src={`${process.env.PUBLIC_URL}/images/umamusume/100301.png`} alt={100301}></img>
+                <div className="absolute w-[36%] -bottom-2 left-0">
+                  <SvgAptitudeWisdom></SvgAptitudeWisdom>
+                </div>
+                <div className="absolute w-[36%] -bottom-2 left-[32.5%]">
+                  <SvgAptitudeRunner></SvgAptitudeRunner>
+                </div>
+                <div className="absolute w-[36%] -bottom-2 right-0">
+                  <SvgAptitudeGreen></SvgAptitudeGreen>
+                </div>
               </div>
               <div className="absolute w-[28%] bottom-[12%] right-[8%]">
                 <img className="rounded-full border border-gray-400" src={`${process.env.PUBLIC_URL}/images/umamusume/100401.png`} alt={100401}></img>
+                <div className="absolute w-[36%] -bottom-2 left-0">
+                  <SvgAptitudeSpeed></SvgAptitudeSpeed>
+                </div>
+                <div className="absolute w-[36%] -bottom-2 left-[32.5%]">
+                  <SvgAptitudeDirt></SvgAptitudeDirt>
+                </div>
+                <div className="absolute w-[36%] -bottom-2 right-0">
+                  <SvgAptitudeGreen></SvgAptitudeGreen>
+                </div>
               </div>
             </div>
           </div>
