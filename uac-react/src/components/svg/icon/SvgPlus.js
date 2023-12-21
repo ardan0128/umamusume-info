fce28f337db7a9e75a69fca8100a659de8abaea3
ardan0128 <jwon1818@daum.net>
2023-12-21 16:32:37 +0900

    [FEAT] fix svg parent

--- uac-react/src/components/svg/icon/SvgPlus.js
+++ uac-react/src/components/svg/icon/SvgPlus.js
@@ -3,16 +3,20 @@ export default function Plus() {
     <svg viewBox="0 0 512 512" width={'100%'} height={'100%'}>
       <defs>
         <linearGradient id="parentEmpty" x1="0%" y1="0%" x2="0%" y2="100%">
-          <stop offset="20%" style={{ stopColor: '#FFFFFF', stopOpacity: 1 }} />
+          <stop offset="40%" style={{ stopColor: '#FFFFFF', stopOpacity: 1 }} />
           <stop offset="100%" style={{ stopColor: '#F1F1F1', stopOpacity: 1 }} />
+        </linearGradient>
+        <linearGradient id="plusIcon" x1="0%" y1="0%" x2="0%" y2="100%">
+          <stop offset="40%" style={{ stopColor: '#B4E20C', stopOpacity: 1 }} />
+          <stop offset="100%" style={{ stopColor: '#79D123', stopOpacity: 1 }} />
         </linearGradient>
         <filter id="insetShadow">
           <feComponentTransfer in="SourceAlpha">
             <feFuncA type="table" tableValues="1 0" />
           </feComponentTransfer>
           <feGaussianBlur stdDeviation="10" />
-          <feOffset dx="0" dy="10" result="offsetblur" />
-          <feFlood flood-color="#D7D7D7" result="color" />
+          <feOffset dx="0" dy="15" result="offsetblur" />
+          <feFlood floodColor="#D7D7D7" result="color" />
           <feComposite in2="offsetblur" operator="in" />
           <feComposite in2="SourceAlpha" operator="in" />
           <feMerge>
@@ -21,13 +25,13 @@ export default function Plus() {
           </feMerge>
         </filter>
 
-        <filter id="textShadow">
+        <filter id="plusShadow">
           <feComponentTransfer in="SourceAlpha">
             <feFuncA type="table" tableValues="1 0" />
           </feComponentTransfer>
           <feGaussianBlur stdDeviation="5" />
           <feOffset dx="0" dy="5" result="offsetblur" />
-          <feFlood flood-color="#D7D7D7" result="color" />
+          <feFlood floodColor="#77C534" result="color" />
           <feComposite in2="offsetblur" operator="in" />
           <feComposite in2="SourceAlpha" operator="in" />
           <feMerge>
@@ -38,6 +42,9 @@ export default function Plus() {
       </defs>
 
       <circle cx={256} cy={256} r={251} stroke="#E4E4E4" strokeWidth={5} fill="url(#parentEmpty)" filter="url(#insetShadow)" />
+      {/* <path d="M180 250 L320 250 M250 180 L250 320" stroke="url(#plusIcon)" strokeWidth={40} filter="url(#plusShadow)" /> */}
+
+      <polygon points="230,180 230,230 180,230 180,270 230,270 230,320 270,320 270,270 320,270 320,230 270,230 270,180" fill="url(#plusIcon)" filter="url(#plusShadow)"></polygon>
     </svg>
   );
 }
