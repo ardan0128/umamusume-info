68b816b58d5f9e16206c6e65235a6a7b190a2db6
ardan0128 <jwon1818@daum.net>
2024-01-03 16:16:31 +0900

    [FEAT] fix modal

--- uac-react/src/components/AptitudeModal.js
+++ uac-react/src/components/AptitudeModal.js
@@ -1,14 +1,40 @@
+import { useEffect, useState } from 'react';
+import './../styles/modal.css';
+import { getAllUmamusume } from '../data/api/Umamusume';
+import Umamusume from './Umamusume';
+
 export default function AptitudeModal({ isOpen, onClose }) {
+  const [loading, setLoading] = useState(true);
+  const [umamusumes, setUmamusumes] = useState();
+
+  useEffect(() => {
+    getAllUmamusume().then(res => {
+      setLoading(false);
+      setUmamusumes(res);
+    });
+  }, []);
+
   return (
     <>
       {isOpen ? (
         <div>
-          <div className="modal-background" onClick={onClose} />
-          <div className="modal-container">
-            <button type="button" onClick={onClose}>
-              close
-            </button>
-            <div>ModalTest</div>
+          <div className="modal-background" onClick={onClose}></div>
+          <div className="modal-container flex flex-col justify-center items-center">
+            <div>Stepper</div>
+            <div>Title</div>
+            <div className="overflow-auto">
+              <ul className="m-2 ml-4 mr-4 grid gap-x-2 gap-y-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 ">
+                {loading
+                  ? 'loading...'
+                  : umamusumes.map(umamusume => {
+                      return (
+                        <li key={umamusume.id}>
+                          <Umamusume id={umamusume.id} name={umamusume.name}></Umamusume>
+                        </li>
+                      );
+                    })}
+              </ul>
+            </div>
           </div>
         </div>
       ) : null}
